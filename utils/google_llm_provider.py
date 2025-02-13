051040d3011ca2f34e20c0fc8ed339b99b128aa1
Lonnie Qin <51523727+LoniQin@users.noreply.github.com>
2025-02-13 10:17:12 +0800

    Optimize code

--- utils/google_llm_provider.py
+++ utils/google_llm_provider.py
@@ -2,9 +2,19 @@ from utils.llm_provider import LLMProvider
 import uuid
 from models import ChatModel, CommonResponse, Message
 import os
-import google.generativeai as genai
 import base64
 from PIL import Image
+from google.genai import types, Client
+
+def get_current_weather(location: str) -> str:
+    """Get the current whether in a given location.
+
+    Args:
+        location: required, The city and state, e.g. San Franciso, CA
+        unit: celsius or fahrenheit
+    """
+    print(f'Called with: {location=}')
+    return "23C"
 
 def convert_base64_to_file(base64_string, fileName):
     images_path = os.path.expanduser("images")
@@ -24,19 +34,8 @@ class GeminiProvider(LLMProvider):
 
     def __init__(self, chat_controller):
         super().__init__(chat_controller)
-        # Create the model
-        generation_config = {
-            "temperature": 1,
-            "top_p": 0.95,
-            "top_k": 40,
-            "max_output_tokens": 8192,
-            "response_mime_type": "text/plain"
-        }
-        genai.configure(api_key=os.environ["GEMINI_API_KEY"])
-        self.model = genai.GenerativeModel(
-            model_name="gemini-2.0-flash",
-            generation_config=generation_config,
-        )
+        self.client = Client(api_key=os.environ["GEMINI_API_KEY"])
+
 
     def get_model(self):
         return ChatModel(
@@ -49,51 +48,58 @@ class GeminiProvider(LLMProvider):
         )
     
     async def execute(self, conversation):
+        messages = []
+        for i in range(len(conversation.messages) - 1):
+            message = conversation.messages[i]
+            role = ""
+            if message.role == "user":
+                role = "user"
+            else:
+                role = "model" 
+            messages.append(types.Part.from_text(message.content))
         user_message = conversation.messages[-1]
+       
         if len(user_message.files) > 0:
-                contents = [user_message.content]
-                for file in user_message.files:
-                    if file.mine_type == "image/jpeg":
-                        file_path = convert_base64_to_file(file.content, file.file_name)
-                        print(f"Saved file to {file_path}")
-                        img = Image.open(file_path)
-                        contents.append(img)
-                try:
-                    response = self.model.generate_content(
-                        contents=contents
-                    )
-                    return CommonResponse(
-                        message="", 
-                        data=[Message(role="assistant", content=response.text)]
-                    ) 
-                except Exception as e:
-                    print("error", e)
-                    return CommonResponse(
-                        message="Error", 
-                        data=[]
-                    ) 
-                
+            contents = [user_message.content]
+            for file in user_message.files:
+                if file.mine_type == "image/jpeg":
+                    file_path = convert_base64_to_file(file.content, file.file_name)
+                    print(f"Saved file to {file_path}")
+                    img = Image.open(file_path)
+                    contents.append(img)
+            try:
+                response = self.client.models.generate_content(
+                    model='gemini-2.0-flash', 
+                    contents=contents
+                )
+                print("Response:", response)
+                return CommonResponse(
+                    message="", 
+                    data=[Message(role="assistant", content=response.candidates[0].content.parts[0].text)]
+                )
+            except Exception as e:
+                print("error", e)
+                return CommonResponse(
+                    message="Error", 
+                    data=[]
+                ) 
         else:
-            history = []
-            for i in range(len(conversation.messages) - 1):
-                message = conversation.messages[i]
-                role = ""
-                if message.role == "user":
-                    role = "user"
-                else:
-                    role = "model" 
-                history.append({
-                    "role": role,
-                    "parts": [
-                        message.content
-                    ]
-                })
-            chat_session = self.model.start_chat(
-                history=history
-            )
-            response = chat_session.send_message(user_message.content)
-            print(response)
-            return CommonResponse(
-                message="", 
-                data=[Message(role="assistant", content=response.text)]
-            ) 
+            try:
+                messages.append(types.Part.from_text(user_message.content))
+                response = self.client.models.generate_content(
+                    model='gemini-2.0-flash', 
+                    contents=messages
+                )
+                print("response:", response)
+                return CommonResponse(
+                    message="", 
+                    data=[Message(role="assistant", content=response.candidates[0].content.parts[0].text)]
+                ) 
+            except Exception as e:
+                print("error", e)
+                return CommonResponse(
+                    message="Error", 
+                    data=[]
+                ) 
+            
+                
